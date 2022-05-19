3e2e863caca5018f20a2831b488fe7aa0be92fb6
Vadim <vad18ru@gmail.com>
2022-05-19 13:51:00 +0400

    5.5. Простое число [#171694 #302603]

--- src/main/java/ru/job4j/loop/CheckPrimeNumber.java
+++ src/main/java/ru/job4j/loop/CheckPrimeNumber.java
@@ -5,13 +5,14 @@ public class CheckPrimeNumber {
         boolean prime = true;
         if (number == 1) {
             prime = false;
-        } else
+        } else {
             for (int i = 2; i < number; i++) {
                 if (number % i == 0) {
                     prime = false;
                     break;
                 }
             }
+        }
         return prime;
     }
 }
