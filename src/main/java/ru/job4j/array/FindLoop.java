4cf1d1d5725e070259ed086cfd77226d82c1ab6b
Vadim <vad18ru@gmail.com>
2022-05-27 09:16:09 +0400

    6.4.2. Слово заканчивается на ... [#156317 #305778]

--- src/main/java/ru/job4j/array/FindLoop.java
+++ src/main/java/ru/job4j/array/FindLoop.java
@@ -1,7 +1,6 @@
 package ru.job4j.array;
-
 public class FindLoop {
-    public static int indexInRange(int[] data, int el, int start, int finish){
+    public static int indexInRange(int[] data, int el, int start, int finish) {
         int rst = -1;
         for (int i = start; i <= finish; i++) {
             if (data[i] == el) {
@@ -10,7 +9,6 @@ public class FindLoop {
             }
         }
         return rst;
-
     }
 
     public static int indexOf(int[] data, int el) {
